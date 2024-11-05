bbaa4f4956535d732fc4eea814e04d90f1dab041
Haris Iftikhar <hiftikh@uwo.ca>
2024-11-05 00:07:59 -0500

    Added task length

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { MantineProvider, Container } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
 import "@mantine/notifications/styles.css";
 import useThemeStore from "./Components/hooks/useThemeStore";
+import useTaskStore from "./Components/hooks/useTaskStore";
 import ToDoList from "./Components/ToDoList";
 import DarkModeToggle from "./Components/DarkModeToggle";
 
 function App() {
   const themeMode = useThemeStore((state) => state.themeMode);
+  const { taskList } = useTaskStore((state) => state);
 
   return (
     <>
@@ -15,8 +17,10 @@ function App() {
         <div className="absolute right-3 top-3">
           <DarkModeToggle />
         </div>
-        <Container size="xs" className="mt-10">
-          <h1 className="text-xl font-bold text-center">To Do List</h1>
+        <Container size="xs" className="mt-10 mb-10">
+          <h1 className="text-xl font-bold text-center">
+            To Do List ({taskList.length})
+          </h1>
           <ToDoList />
         </Container>
         <Notifications />
